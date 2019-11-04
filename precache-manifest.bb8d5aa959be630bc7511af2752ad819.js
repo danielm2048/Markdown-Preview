058a2f9c637e44bde27ddc4d3fe894999e6c80c9
danielm2048 <danielmimoun98@gmail.com>
2019-11-05 01:53:12 +0200

    Updates

---
+++ precache-manifest.bb8d5aa959be630bc7511af2752ad819.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a448ddab5114deb63d9fc5b6642688d7",
+    "url": "/Markdown-Preview/index.html"
+  },
+  {
+    "revision": "033dc06ea7460aacd290",
+    "url": "/Markdown-Preview/static/css/main.f4aa18b1.chunk.css"
+  },
+  {
+    "revision": "3cc7ed761288aee755c4",
+    "url": "/Markdown-Preview/static/js/2.dcd08426.chunk.js"
+  },
+  {
+    "revision": "033dc06ea7460aacd290",
+    "url": "/Markdown-Preview/static/js/main.10091540.chunk.js"
+  },
+  {
+    "revision": "712bbb7b18bf20b72fe3",
+    "url": "/Markdown-Preview/static/js/runtime-main.e0802eac.js"
+  }
+]);
